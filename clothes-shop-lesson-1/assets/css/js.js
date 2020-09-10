17115bd8ab816e1c79358d9b48ee1164ef40eb90
Leonid <tugrew@inbox.ru>
2020-09-10 15:30:14 +0500

    Верстка с макета. в блоке с отзывами использовался слик слайдер, также для всей вёрстки применялся фреймворк bootstrap.

---
+++ clothes-shop-lesson-1/assets/css/js.js
@@ -0,0 +1,13 @@
+
+$( document ).ready(function() {
+   $('.reviews-slider-js').slick({
+		slidesToShow: 1,
+ 	 	slidesToScroll: 1,
+ 	 	autoplay: true,
+ 	 	autoplaySpeed: 3000,
+ 	 	dots: true,
+ 	 	arrows: false,
+ 	 	centerMode: false,
+	});
+});
+	
